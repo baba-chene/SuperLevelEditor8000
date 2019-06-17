31353358857cfb6b2397a551f3164e66af2b0482
Ulysse JOBARD <ulysse.jobard@telecom-paristech.fr>
2019-06-17 15:59:35 +0200

    Ulysse120000

--- levelEditor/src/ui/FileMenuBar.java
+++ levelEditor/src/ui/FileMenuBar.java
@@ -1,5 +1,16 @@
 package ui;
 
-public class FileMenuBar {
+import javax.swing.*;
+
+public class FileMenuBar extends JMenuBar{
+	
+	private FileMenu fileMenu;
+	
+	public FileMenuBar(EditorFrame editorFrame) {
+		
+		super();
+		
+		add(fileMenu = new FileMenu(editorFrame));
+	}
 
 }
--- levelEditor/src/ui/ObjectList.java
+++ levelEditor/src/ui/ObjectList.java
@@ -1,8 +1,43 @@
 package ui;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class ObjectList extends JFrame {
+public class ObjectList extends JPanel {
 
+	private final EditorFrame editorFrame;
+	private final ObjectButtonGroup objectButtonGroup;
 	
+	public ObjectList (EditorFrame editorFrame) {
+		
+		super();
+		this.editorFrame= editorFrame;
+		setLayout(new GridLayout(50, 2));
+		setPreferredSize(new Dimension(200, 800));
+		
+		
+		//Initialisation des choix
+		ArrayList<ObjectButton> radioButtons = new ArrayList<ObjectButton>();
+		
+		radioButtons.add(new ObjectButton("empty", editorFrame, true));
+		radioButtons.add(new ObjectButton("baba", editorFrame, false));
+		radioButtons.add(new ObjectButton("wall", editorFrame, false));
+		radioButtons.add(new ObjectButton("water", editorFrame, false));
+		radioButtons.add(new ObjectButton("rock", editorFrame, false));
+		radioButtons.add(new ObjectButton("lava", editorFrame, false));
+		radioButtons.add(new ObjectButton("flag", editorFrame, false));
+		
+		ObjectButtonGroup radioButtonGroup = new ObjectButtonGroup(radioButtons);
+		this.objectButtonGroup = radioButtonGroup;
+	
+
+		
+		for (ObjectButton e : radioButtons){
+			add(e);
+		}
+	}
 }
