006844655611ab519607e962391a0eac636dd1f8
Clement Ducros <cducros@c127-19.enst.fr>
2019-06-17 15:45:40 +0200

    Box et LevelPanel

--- levelEditor/src/ui/Box.java
+++ levelEditor/src/ui/Box.java
@@ -1,5 +1,88 @@
-package ui;
+ackage ui;
 
-public class Box {
+import java.awt.*;
+import java.awt.geom.*;
 
+import level.Level;
+
+
+@SuppressWarnings("serial")
+public final class Box extends Rectangle2D.Float 
+{
+	private int abscissOrigin;
+	private int ordinateOrigin;
+	private int width;
+	private int height;
+	
+	
+	public Box(int x, int y, int w, int h)
+	{
+		super(x,y,w,h);
+		this.abscissOrigin = x;
+		this.ordinateOrigin = y;
+		this.width = w;
+		this.height = h;
+		
+	}
+	
+	public final void paintRockBox(Graphics g)
+	// Change to WallBox
+	{	
+		g.setColor(Color.GREEN);
+		g.drawRect(abscissOrigin, ordinateOrigin, width, height);
+		g.fillRect(abscissOrigin, ordinateOrigin, width, height);
+	}
+	
+	public final void paintEmptyBox(Graphics g)
+	//Change to EmptyBox
+	{
+		g.setColor(Color.BLACK);
+		g.drawRect(abscissOrigin, ordinateOrigin, width, height);
+		g.fillRect(abscissOrigin, ordinateOrigin, width, height);
+	}
+	
+	public final void paintBabaBox(Graphics g)
+	//Change to  DepartureBox
+	{
+		g.setColor(Color.WHITE);
+		g.drawRect(abscissOrigin, ordinateOrigin, width, height);
+		g.fillRect(abscissOrigin, ordinateOrigin, width, height);	
+	}
+	
+	public final void paintWaterBox(Graphics g)
+	//Change to  ArrivalBox
+	{
+		g.setColor(Color.BLUE);
+		g.drawRect(abscissOrigin, ordinateOrigin, width, height);
+		g.fillRect(abscissOrigin, ordinateOrigin, width, height);	
+	}
+	
+	
+	public final void paintLavaBox(Graphics g)
+	//Change to  ArrivalBox
+	{
+		g.setColor(Color.RED);
+		g.drawRect(abscissOrigin, ordinateOrigin, width, height);
+		g.fillRect(abscissOrigin, ordinateOrigin, width, height);	
+	}
+	
+	
+	public final void paintFlagBox(Graphics g)
+	//Change to  ArrivalBox
+	{
+		g.setColor(Color.YELLOW);
+		g.drawRect(abscissOrigin, ordinateOrigin, width, height);
+		g.fillRect(abscissOrigin, ordinateOrigin, width, height);	
+	}
+
+	public int getAbscissOrigin()
+	{
+		return abscissOrigin;
+	}
+
+	public int getOrdinateOrigin()
+	{
+		return ordinateOrigin;
+	}
 }
+
--- levelEditor/src/ui/LevelPanel.java
+++ levelEditor/src/ui/LevelPanel.java
@@ -1,5 +1,153 @@
 package ui;
 
-public class LevelPanel {
+import javax.swing.*;
 
-}
+import level.Level;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+
+import java.io.File;
+import java.util.*;
+
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Set;
+
+@SuppressWarnings("serial")
+public final class LevelPanel extends JPanel implements MouseListener, MouseMotionListener
+{
+	private final LevelPreview levelPreview;
+	private Level level;
+	private Box[][] listBox;
+	int width;
+	int height;
+	int margin;
+	
+	public LevelPanel(LevelPreview levelPreview, Level level)
+	{
+		super();
+		this.levelPreview = levelPreview;
+		setBackground(Color.WHITE);
+		this.level = level;
+		
+		setPreferredSize(new Dimension(512,512));
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		
+		width = 20;
+		height = 20;		
+		margin = 10;
+						
+				
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		for (int i=0;i<level.hauteur;i++) {
+			for (int j = 0; j< level.largeur; j++) {
+				
+				paintBox(g,level.getMap(),i,j);
+				
+				
+			}
+			
+		}
+		
+	}
+	private void createBoxes() {
+
+		
+		for (int i = 0; i < level.hauteur; i++) {
+			
+			for (int j = 0; j < level.largeur; j++) {
+				listBox[i][j] = new Box (margin+i*(height+1),margin+j*(width+1),width,height);
+	
+			}
+		}
+		
+	}
+	
+	
+	
+	private void paintBox(Graphics g, String[][] map, int i, int j) {
+			
+		switch((map[i][j]) {
+		
+		case "rock" : 
+			listBox[i][j].paintRockBox(g);
+		case "lava" : 
+			listBox[i][j].paintLavaBox(g);
+		case "flag" : 
+			listBox[i][j].paintFlagBox(g);
+		case "baba" :
+			listBox[i][j].paintBabaBox(g);
+		case "water":
+			listBox[i][j].paintWaterBox(g);
+		
+		}	
+	}
+	
+	
+	public void notifyForUpdate()
+	{
+		repaint();
+	}
+
+	
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
