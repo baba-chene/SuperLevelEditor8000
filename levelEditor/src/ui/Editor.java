f04789048bab946747a06730030244005bcee7d3
Ulysse JOBARD <ulysse.jobard@telecom-paristech.fr>
2019-06-18 15:37:39 +0200

    Color for block added

--- levelEditor/src/ui/Editor.java
+++ levelEditor/src/ui/Editor.java
@@ -5,7 +5,10 @@ import level.Level;
 public class Editor {
 
 	public static String[] elements = {"empty","baba","keke","rock","wall","water","lava","flag","grass"};
-	public static String[] keyWords = {"is","has","you","us","win","defeat","push","block","sink","death"};
+	public static String[] keyWords = {"is","has","you","us","win","defeat","push","block","sink","death","move"};
+	public static String[] colorElements = {"#000000","#FFFFFF","#FFB3B3","#774A00","#8A8A8A","#00DEFF","#FF4D00","#FFE400","#009835"};
+	public static String[] colorTextElements = {"#A700FF","#A700FF","#A700FF","#A700FF","#A700FF","#A700FF","#A700FF","#A700FF","#A700FF",};
+	public static String[] colorKeyWords = {"#FF00C8","#FF00C8","#0C00FF","#0C00FF","#D4FF00","#930021","#43AC52","#6C5833","#000BAD","#660037","#05B87E"};
 	
 	
 	private Level level;
@@ -16,11 +19,5 @@ public class Editor {
 		editorFrame= new EditorFrame(this,level);
 	};
 	
-	public void save(String absolutePath) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
-	
+
 }
